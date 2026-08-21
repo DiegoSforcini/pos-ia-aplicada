7257f5cb78d5c8caa12693e57f3d58b3be6ff919
DiegoSforcini <diegossforcini@gmail.com>
2026-08-20 22:11:03 -0300

    feat: finalizando módulo 2

--- modulo-02-apis-de-ia-generativa-e-prompt-engineering/exemplo-02-langchain-intro/tests/router.e2e.test.ts
+++ modulo-02-apis-de-ia-generativa-e-prompt-engineering/exemplo-02-langchain-intro/tests/router.e2e.test.ts
@@ -19,3 +19,39 @@ test('command upper transforms message into UPPERCASE', async () => {
     assert.equal(response.statusCode, 200);
     assert.equal(response.body, expected);
 })
+
+test('command upper transforms message into LOWERCASE', async () => {
+
+    const app = createServer()
+    const msg = "MAKE THIS MESSAGE LOWER PLEASE!";
+    const expected = msg.toLocaleLowerCase();
+
+    const response = await app.inject({
+        method: 'POST',
+        url: '/chat',
+        body: {
+            question: msg
+        }
+    });
+
+    assert.equal(response.statusCode, 200);
+    assert.equal(response.body, expected);
+})
+
+test('command upper transforms message into UNKNOWN', async () => {
+
+    const app = createServer()
+    const msg = "HEY THERE!";
+    const expected = "Unknown command. Try 'make this uppercase' or 'convert to lowercase'";
+
+    const response = await app.inject({
+        method: 'POST',
+        url: '/chat',
+        body: {
+            question: msg
+        }
+    });
+
+    assert.equal(response.statusCode, 200);
+    assert.equal(response.body, expected);
+})
