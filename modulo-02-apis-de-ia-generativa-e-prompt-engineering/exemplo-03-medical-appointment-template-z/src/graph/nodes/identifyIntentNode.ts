cf255b8f88bd4c93e5de391fd7c58254cf16787c
DiegoSforcini <diegossforcini@gmail.com>
2026-08-30 11:23:32 -0300

    feat: atualizando até a aula 2

--- modulo-02-apis-de-ia-generativa-e-prompt-engineering/exemplo-03-medical-appointment-template-z/src/graph/nodes/identifyIntentNode.ts
+++ modulo-02-apis-de-ia-generativa-e-prompt-engineering/exemplo-03-medical-appointment-template-z/src/graph/nodes/identifyIntentNode.ts
@@ -1,11 +1,21 @@
+import { getSystemPrompt, getUserPromptTemplate, IntentSchema } from '../../prompts/v1/identifyIntent.ts';
+import { professionals } from '../../services/appointmentService.ts';
+import { OpenRouterService } from '../../services/openRouterService.ts';
 import type { GraphState } from '../graph.ts';
 
-export function createIdentifyIntentNode() {
+export function createIdentifyIntentNode(llmClient: OpenRouterService) {
   return async (state: GraphState): Promise<GraphState> => {
     console.log(`🔍 Identifying intent...`);
    const input = state.messages.at(-1)!.text;
 
     try {
+      const systemPrompt = getSystemPrompt(professionals);
+      const userPrompt = getUserPromptTemplate(input);
+      const result = await llmClient.generateStructured(
+        systemPrompt,
+        userPrompt,
+        IntentSchema,
+      );
 
       return {
         ...state,
